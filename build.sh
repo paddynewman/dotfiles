8b0a24369696e61f385a5c45340681e3acce5105
Paddy Newman <paddynewman@gmail.com>
2019-11-01 12:25:29 +0000

    Add a .curlrc file and hook it into build.sh.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files='.bash_profile .bashrc .vimrc .tmux.conf .gitconfig'
+files='.bash_profile .bashrc .vimrc .tmux.conf .gitconfig .curlrc'
 
 for file in $files; do
 	(set -x; cp $file ~/$file)
