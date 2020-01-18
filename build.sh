418c81f28b2e0b7cbfec17c7c1b78fc9fd68b227
Paddy Newman <paddy.newman@rakuten.com>
2020-01-18 14:46:54 +0000

    Add a global .gitignore.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files='.bash_profile .bashrc .vimrc .tmux.conf .gitconfig .curlrc'
+files='.bash_profile .bashrc .vimrc .tmux.conf .gitconfig .gitignore .curlrc'
 
 for file in $files; do
 	(set -x; cp $file ~/$file)
